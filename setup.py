22291b79e16350246fcfd4ead606cc0ed08cc2ec
Mohammed-Althaf <mohamedalthaf694@gmail.com>
2024-12-07 08:09:19 +0400

    desc update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="NashUtils", 
-    version="1.1.0", 
+    version="1.0.1", 
     description="This package contains Utlitiy tools for Python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
